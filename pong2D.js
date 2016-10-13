4781828938e8c5e93f27baeabba466fda1983864
nussbam <marc.nussbaumer@stud.hslu.ch>
2016-10-13 18:15:51 +0200

    Fixed code that showed warnings

--- pong2D.js
+++ pong2D.js
@@ -11,18 +11,17 @@ window.onload = startup;
 var canvas;
 var gl;
 var aVertexPositionID;
-var buffer;
-var karthBuffer;
 var playerLeftBuffer;
 var playerRightBuffer;
 var middleLineBuffer;
 var ballBuffer;
-var colorBuffer;
-var combinedBuffer;
 var shaderProgram;
 var uColorPositionId ;
 var uModelViewMatrix;
 var orthoMatrix;
+var ballCounter=0;
+var lastTimestamp;
+var pixelSpeed=20;
 
 /**
  * Connects the shader-variables with javascript.
@@ -35,7 +34,7 @@ function setupAttributes(){
 
 
 
-function setupLeftBuffer(){
+function setupLeftPlayerBuffer(){
 
     playerLeftBuffer = gl.createBuffer();
 
@@ -45,7 +44,7 @@ function setupLeftBuffer(){
         15,0,
         0,70,
         15,70,
-        0,0,
+        0,0
 
         //Second square positioned bottom left
 
@@ -55,7 +54,7 @@ function setupLeftBuffer(){
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
-function setupRightBuffer(){
+function setupRightPlayerBuffer(){
 
     playerRightBuffer = gl.createBuffer();
 
@@ -65,7 +64,7 @@ function setupRightBuffer(){
         15,0,
         0,70,
         15,70,
-        0,0,
+        0,0
 
         //Second square positioned bottom left
 
@@ -85,7 +84,7 @@ function setupMiddleLineBuffer(){
         402,0,
         398,800,
         402,800,
-        398,0,
+        398,0
 
         //Second square positioned bottom left
 
@@ -105,7 +104,7 @@ function setupBallBuffer(){
         15,0,
         0,15,
         15,15,
-        0,0,
+        0,0
 
         //Second square positioned bottom left
 
@@ -114,116 +113,6 @@ function setupBallBuffer(){
     gl.bindBuffer(gl.ARRAY_BUFFER, ballBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
-
-/**
- * Fills the buffer with values to draw two squares.
- * Allowed vertices-values are in the default grid between -1 and 1.
- */
-function setupKarthBuffers(){
-    karthBuffer = gl.createBuffer();
-
-    var vertices = [
-        //First square positioned top right
-        0,0,
-        20,0,
-        0,20,
-        20,20,
-        0,0,
-
-        //Second square positioned bottom left
-
-
-    ];
-    gl.bindBuffer(gl.ARRAY_BUFFER, karthBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
-/**
- * Fills the buffer with values to draw two squares.
- * Allowed vertices-values are in the default grid between -1 and 1.
- */
-function setupBuffers(){
-    buffer = gl.createBuffer();
-
-    var vertices = [
-        //First square positioned top right
-        0,0,
-        0.5,0,
-        0,0.5,
-        0.5,0.5,
-        0,0,
-
-        //Second square positioned bottom left
-
-        0,0,
-        -0.5,-0,
-        0, -0.5,
-        -0.5,-0.5,
-        0, 0
-
-    ];
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
-/**
- * Seperate buffer for just colors
- */
-function setupBufferColor(){
-    colorBuffer = gl.createBuffer();
-
-    var vertices = [
-        1,1,1,1,
-        0.5,0.5,0.5,1,
-        0,0.5,0.5,1,
-        1,1,1,1,
-
-
-        0.5,0.5,0.5,1,
-        0.2, 0.2, 0.2, 0.2,
-        0.4,0.2,0.2,0.4,
-        0.2, 0.3,0.3,0.3
-
-    ];
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
-function setupCombinedBuffer(){
-    combinedBuffer = gl.createBuffer();
-    var points = [-0.5,0.5];
-
-
-    var vertices = [
-        /*0,0,1,1,1,1,
-        0.5,0,0.5,0.5,0.5,1,
-        0,0.5,0,0.5,0.5,1,
-        0.5,0.5,1,1,1,1,*/
-
-        0,0, Math.random(),Math.random(),Math.random(),Math.random(),
-        0.5,0, Math.random(),Math.random(),Math.random(),Math.random(),
-        0,0.5, Math.random(),Math.random(),Math.random(),Math.random(),
-        0.5,0.5, Math.random(),Math.random(),Math.random(),Math.random(),
-
-        -0.5,-0.5, Math.random(),Math.random(),Math.random(),Math.random(),
-        0,-0.5, Math.random(),Math.random(),Math.random(),Math.random(),
-        -0.5,-1, Math.random(),Math.random(),Math.random(),Math.random(),
-        0,-1, Math.random(),Math.random(),Math.random(),Math.random()
-
-        /*-0.5,-0.5,0.5,0.5,0.5,1,
-        0,-0.5,0.2, 0.2, 0.2, 0.2,
-        -0.5,-1,0.4,0.2,0.2,0.4,
-        0,-1,0.2, 0.3,0.3,0.3
-        */
-    ];
-
-
-
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, combinedBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
 
 function startup(){
     canvas = document.getElementById("gameCanvas");
@@ -246,8 +135,8 @@ function initGL(){
 
     shaderProgram = loadAndCompileShaders(gl,"VertexShader.shader","FragmentShader.shader");
     setupAttributes();
-    setupRightBuffer();
-    setupLeftBuffer();
+    setupRightPlayerBuffer();
+    setupLeftPlayerBuffer();
     setupMiddleLineBuffer();
     setupBallBuffer();
     orthoMatrix = mat4.create();
@@ -261,13 +150,19 @@ function initGL(){
 }
 
 function drawAnimated(timeStamp){
-
-    setupCombinedBuffer();
+    if(!lastTimestamp){
+        lastTimestamp = timeStamp;
+    }
+    var detlaTime = timeStamp - lastTimestamp;
+    lastTimestamp = timeStamp;
     draw();
     window.requestAnimationFrame(drawAnimated);
 }
 
 function draw(){
+    ballCounter++;
+
+
     var temp = mat4.create();
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -295,7 +190,7 @@ function draw(){
     gl.bindBuffer(gl.ARRAY_BUFFER, ballBuffer);
     gl.vertexAttribPointer(aVertexPositionID, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aVertexPositionID);
-    mat4.translate(temp,orthoMatrix,[0,0,0]);
+    mat4.translate(temp,orthoMatrix,[ballCounter,0,0]);
     gl.uniformMatrix4fv(uModelViewMatrix,false,temp);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
